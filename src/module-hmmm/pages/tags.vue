3e20e030559ad203f830e388f8fab60d51c128bf
joker <1771693160@qq.com>
2019-09-01 19:03:15 +0800

    学科列表项目基本完成

--- src/module-hmmm/pages/tags.vue
+++ src/module-hmmm/pages/tags.vue
@@ -71,7 +71,7 @@
     </el-table-column>
   </el-table>
 <!-- 设置分页界面 -->
-<el-row justify="center" type="flex">
+<el-row justify="center" type="flex" style="margin-top:30px">
 <el-pagination
  background
  @current-change="handleCurrentChange"
@@ -82,11 +82,23 @@
  layout="sizes, prev, pager, next, jumper"
  ></el-pagination>
 </el-row>
+ <!-- 弹出框 新增/修改操作实现 -->
+ <el-dialog title="标签" :visible.sync="dialogFormVisible">
+  <el-form style="margin-top:30px">
+    <el-form-item label="标签名称" label-width="80px">
+      <el-input autocomplete="off" style="width:300px" placeholder="请输入" v-model="formData.tagName"></el-input>
+    </el-form-item>
+  </el-form>
+     <div slot="footer" class="dialog-footer">
+    <el-button @click="dialogFormVisible = false">取 消</el-button>
+    <el-button type="primary" @click="orDirectory">确 定</el-button>
+  </div>
+</el-dialog>
   </el-card>
 </template>
 
 <script>
-import {list, detail, add, updata, remove, removeState} from '@/api/hmmm/tags'
+import {list, detail, add, update, remove, removeState} from '@/api/hmmm/tags'
 export default {
   name: 'TagsList',
   data() {
@@ -98,24 +110,47 @@ export default {
         page: 1,
         pagesize: 10
       },
-      dataList: []
+      dataList: [],
+      formData: {
+      id: '',
+      subjectID: '',
+      tagName: ''
+      }
     }
   },
   methods: {
+    alert(data) { // 点击修改按钮  显示弹出框
+      this.dialogFormVisible = true
+      this.flag = false
+      this.formData.id = data.id
+      this.formData.tagName = data.tagName
+    },
+    async orDirectory() { // 弹出框确定按钮  进行项目修改/新增操作
+    console.log(this.formData)
+     this.flag ? await add(this.formData) : await update(this.formData)
+     this.dialogFormVisible = false
+     this.formData.tagName = ''
+     await this.getList()
+      this.$message({
+          message: `恭喜你,${this.flag ? '标签添加成功' : '标签修改成功'}`,
+          type: 'success'
+        })
+    },
     // 获取列表数据
    async getList() {
       const { data: res } = await list(this.requestParameters)
       // 获取数据给dataList
       this.dataList = res.items
-      console.log(this.dataList)
       // 获取总条数
       this.total = res.counts
+      console.log(this.dataList)
     },
     handleCurrentChange(newPage) {
     }
   },
   created() {
     this.getList()
+    this.formData.subjectID = Number(this.$route.params.id)
   }, 
 filters: {
   transformName(value) {
